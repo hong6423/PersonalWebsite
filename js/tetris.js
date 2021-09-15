96d98f77c09f9e7b598078d680abde71b5ef04a2
Choi <hong30326@gmail.com>
2021-09-14 22:29:52 -0700

    Create blocks format

--- js/tetris.js
+++ js/tetris.js
@@ -8,20 +8,39 @@ const GAME_ROW = 20;
 const GAME_COLS = 10;
 
 //variables
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+
+const BLOCKS ={
+    tree: [
+        [[2,1],[0,1],[1,0],[1,1]],
+        [],
+        [],
+        [],
+    ]
+}
+
+
+const movingItem = {
+    type: "tree",
+    direction: 0,
+    top: 10,
+    left: 3,
+};
 
 
 init()
 
 //functions
 function init(){
+    tempMovingItem = { ...movingItem};
     for(let i = 0; i < GAME_ROW; i++){
         prependNewLine()
     }
+    renderBlocks()
 }
-
-
-
-
 function prependNewLine(){
     const li = document.createElement("li");
     const ul = document.createElement("ul");
@@ -31,4 +50,15 @@ function prependNewLine(){
     }
     li.prepend(ul)
     playground.prepend(li)
+}
+function renderBlocks(){
+    const{ type, direction, top, left} = tempMovingItem;
+    //console.log(BLOCKS[type][direction])
+    BLOCKS[type][direction].forEach(block =>{
+        const x = block[0] + left;
+        const y = block[1] + top;
+        const target = playground.childNodes[y].childNodes[0].childNodes[x];
+        target.classList.add(type)
+    })
+
 }
